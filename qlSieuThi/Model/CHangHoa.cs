675b9ef297fe144ee4eec9bbfa055cc966974c66
LuuDucHieu <henry_hieu1996@yahoo.com.vn>
2017-02-06 19:56:50 +0700

    adding sqlite

---
+++ qlSieuThi/Model/CHangHoa.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qlSieuThi.Model
+{
+    class CHangHoa
+    {
+        //scan code
+        [PrimaryKey]
+        private string ma;
+        private string ten;
+        private int soluong;
+        private string tencongty;
+
+        CHangHoa()
+        {
+
+        }
+
+        CHangHoa(string Ma, string Ten, int Soluong, string Tencongty)
+        {
+            ma = Ma;
+            ten = Ten;
+            soluong = Soluong;
+            tencongty = Tencongty;
+        }
+
+        CHangHoa(string Ma, string Ten, int Soluong)
+        {
+            ma = Ma;
+            ten = Ten;
+            soluong = Soluong;
+        }
+
+        public string Ma
+        {
+            get
+            {
+                return ma;
+            }
+
+            set
+            {
+                ma = value;
+            }
+        }
+
+        public string Ten
+        {
+            get
+            {
+                return ten;
+            }
+
+            set
+            {
+                ten = value;
+            }
+        }
+
+        public int Soluong
+        {
+            get
+            {
+                return soluong;
+            }
+
+            set
+            {
+                soluong = value;
+            }
+        }
+
+        public string Tencongty
+        {
+            get
+            {
+                return tencongty;
+            }
+
+            set
+            {
+                tencongty = value;
+            }
+        }
+    }
+}
---
+++ qlSieuThi/Model/CNhanVien.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qlSieuThi.Model
+{
+    class CNhanVien
+    {
+        private string ma;
+        private string ten;
+        private bool gioitinh;
+
+        //Datetime sau nay
+        private string ngaysinh;
+
+        private string diachi;
+        private string sodienthoai;
+       
+    }
+}
